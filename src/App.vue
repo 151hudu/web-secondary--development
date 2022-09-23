43cf51bc7f887db0cc3861bc518a88cc4de8f8f5
zhuwenchao <jackcz125a@126.com>
2022-09-23 18:43:36 +0800

    9.23 style2

--- src/App.vue
+++ src/App.vue
@@ -284,6 +284,32 @@ export default {
       return date.getFullYear() + "-" + month;
       //返回格式：yyyy-MM-dd hh:mm
     },
+    getSummaries(param) {
+      const { columns, data } = param;
+      const sums = [];
+      columns.forEach((column, index) => {
+        if (index === 0) {
+          sums[index] = "合计";
+          return;
+        }
+        const values = data.map((item) => Number(item[column.property]));
+        if (!values.every((value) => isNaN(value))) {
+          sums[index] = values.reduce((prev, curr) => {
+            const value = Number(curr);
+            if (!isNaN(value)) {
+              return (prev + curr).toFixed(2);
+            } else {
+              return prev;
+            }
+          }, 0);
+          sums[index] += " 元";
+        } else {
+          sums[index] = "N/A";
+        }
+      });
+
+      return sums;
+    },
     exportExcel() {
       let message = {};
       if (this.stateR == "month") {

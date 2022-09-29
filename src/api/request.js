a3c5b28f3d8aff5e438008712ba592a0342abd4f
zhuwenchao <jackcz125a@126.com>
2022-09-29 16:52:44 +0800

    Revert "9.29fixed"
    This reverts commit 75de4da4eb8bc141b7f570d53873c19f4a3ab3c1.

--- src/api/request.js
+++ src/api/request.js
@@ -10,7 +10,7 @@ if (process.env.NODE_ENV === "development") {
   apiContextPath = "/api";
 }
 const instance = axios.create({
-  baseURL: `${window.apiContextPathPrefix || process.env.REACT_APP_API}/sdata/rest`,
+  baseURL: `${process.env.REACT_APP_API}/sdata/rest`,
   timeout: 60000,
   validateStatus: function (status) {
     return status >= 200 && status < 300; // default
